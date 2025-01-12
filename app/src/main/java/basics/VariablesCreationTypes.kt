be071aab5ef6917e49eda0870c013ff95d2757c8
enesray <iletisim.enesray@gmail.com>
2025-01-12 15:19:46 +0300

    Added VariablesCreationTypes.kt

---
+++ app/src/main/java/basics/VariablesCreationTypes.kt
@@ -0,0 +1,34 @@
+package basics
+//Değişken Oluşturma Çeşitleri
+
+fun main() {
+
+    //Ornek 1
+
+    var sayi = 10
+
+    //Ornek 2
+    var fiyat = 12.99
+
+    fiyat = 10.99
+
+    println(fiyat)
+
+    //Ornek 3
+    var s1 = 80
+    var s2 = 70
+    var toplam = s1 + s2
+    println(toplam)
+
+    //Ornek 4 - Type Safety – Tür Güvenliği
+    //Oluşturduğunuz değişkene farklı türde değişken atayamazsınız.
+    var sonuc = 100
+    sonuc = 50
+
+    /*
+    Bu durumlar hatalıdır
+    Değişken INT biz double veya string atayamayız.
+    sonuc = 50.0
+    sonuc = "Enes"
+    */
+}
